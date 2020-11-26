7e317e68161c1ad98fd6c0098b8038192ee44a16
ernest0713 <49123083+ernest0713@users.noreply.github.com>
2020-11-26 15:45:07 +0800

    Update 2020-11-26T07:45:00.982Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
   console.log('HesSchool Hello!');
-  var pathname = window.location.pathname.split('/')[1].split('.')[0];
+  var pathname = window.location.pathname.split('/').pop().split('.')[0];
   $("#".concat(pathname)).addClass('active');
 });
 //# sourceMappingURL=all.js.map
