73ac4a389ba7cbc1974beffbb5624e950c8a58ba
ernest0713 <49123083+ernest0713@users.noreply.github.com>
2020-11-26 15:37:47 +0800

    Update 2020-11-26T07:37:42.860Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,8 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  var pathname = window.location.pathname.split('/')[1].split('.')[0];
+  $("#".concat(pathname)).addClass('active');
 });
 //# sourceMappingURL=all.js.map
