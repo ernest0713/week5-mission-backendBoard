3fac3b1f31c8dc56a5bae371572298558a3b11b4
ernest0713 <49123083+ernest0713@users.noreply.github.com>
2020-11-26 15:44:37 +0800

    fixed index js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
   console.log('HesSchool Hello!');
-  var pathname = window.location.pathname.split('/')[1].split('.')[0];
+  var pathname = window.location.pathname.split('/').pop().split('.')[0];
   $(`#${pathname}`).addClass('active');
 });
