8c3a7b03675151383d4b13b8ebf7eec8855026d6
ernest0713 <49123083+ernest0713@users.noreply.github.com>
2020-11-26 15:36:43 +0800

    fixed nav link class

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,5 @@
-console.log('Hello!');
-
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+  var pathname = window.location.pathname.split('/')[1].split('.')[0];
+  $(`#${pathname}`).addClass('active');
 });
